87963e0afd36c682df59454359389f59eca607fd
miguelmetzuade <mmetz@uade.edu.ar>
2025-06-27 17:06:39 -0300

    Add file to read historical data

---
+++ data_storage/read_historical_data.py
@@ -0,0 +1,24 @@
+try:
+    from handler_files.csv_manager import confread_csv_as_matrixig
+except (ImportError, ModuleNotFoundError):
+    import os
+    import sys
+    path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    sys.path.insert(1, path)
+
+from handler_files.csv_manager import read_csv_as_matrix
+
+
+def __get_historical_data(file_path: str) -> list:
+    """Retrieves historical data from a CSV file.
+
+    Args:
+        file_path (str): The path to the CSV file.
+
+    Returns:
+        list: A list of lists representing the CSV file content.
+    """
+    content = read_csv_as_matrix(file_path)
+    if not content:
+        raise Exception(f"The file {file_path} is empty or does not contain valid data.")
+    return content
